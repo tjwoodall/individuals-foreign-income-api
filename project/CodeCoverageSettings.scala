b94bcaf3094343c09a7d25474cd495ef24285690
Robert Oman <182665093+robomancap@users.noreply.github.com>
2026-04-21 09:31:40 +0100

    MTDSA-33651 - Remove references to HATEOAS (#57)

--- project/CodeCoverageSettings.scala
+++ project/CodeCoverageSettings.scala
@@ -33,7 +33,7 @@ object CodeCoverageSettings {
 
   val settings: Seq[Setting[?]] = Seq(
     ScoverageKeys.coverageExcludedPackages := excludedPackages.mkString(";"),
-    ScoverageKeys.coverageMinimumStmtTotal := 95,
+    ScoverageKeys.coverageMinimumStmtTotal := 97,
     ScoverageKeys.coverageFailOnMinimum := true,
     ScoverageKeys.coverageHighlighting := true
   )
